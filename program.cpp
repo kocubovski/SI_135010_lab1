a72a516e901c97f1eef3d5855ef1663d315484d5
unknown <kochubovski.vanja@students.finki.ukim.mk>
2019-04-04 10:30:25 +0200

    functions implemented

--- program.cpp
+++ program.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 void printMyName () {
 	//TODO
+	cout<<"Vanja Kochubovski";
 }
 
 void printMyIndex () {
 	//TODO
+	cout<<"135010";
 }
 
 int main () {
 	//TODO
+	printMyName ();
+	printMyIndex ();
 }
